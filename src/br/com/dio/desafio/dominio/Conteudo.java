1d680d3dd152c562813ea611eb8305bcc111c34c
arthuranael1 <arthuranael1@gmail.com>
2024-11-07 23:57:31 -0300

    Update Conteudo.java

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,11 +1,14 @@
 package br.com.dio.desafio.dominio;
 
+/**
+ * Classe abstrata que representa um Conteúdo no Bootcamp.
+ * Define atributos comuns como título e descrição, e um método abstrato para calcular XP.
+ */
 public abstract class Conteudo {
-
     protected static final double XP_PADRAO = 10d;
 
-    private String titulo;
-    private String descricao;
+    protected String titulo;
+    protected String descricao;
 
     public abstract double calcularXp();
 
